990a822d5c755809dac5e2174ae386d4f7954505
Glassy Iris <Coloredflowers@outlook.com>
2024-01-09 17:02:12 +0800

    add: flake.lock

--- flake.nix
+++ flake.nix
@@ -6,21 +6,19 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvimdots.url = "github:ayamir/nvimdots";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  {
+  outputs = inputs@{ self, nix-darwin, nixpkgs, ... }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#irisdeMac-Pro
-    darwinConfigurations."irisdeMac-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        ./mac
-      ];
-    };
+    darwinConfigurations."irisdeMac-Pro" =
+      nix-darwin.lib.darwinSystem { 
+        modules = [ 
+          ./mac 
+        ]; 
+      };
 
     # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."irisdeMac-Pro".pkgs;
