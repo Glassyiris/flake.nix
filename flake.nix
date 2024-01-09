0ff20ccb1020d08a96ce4fc9d949805a55886296
Glassy Iris <Coloredflowers@outlook.com>
2024-01-09 14:21:05 +0800

    add: flake.lock

--- flake.nix
+++ flake.nix
@@ -8,47 +8,13 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.vim
-          pkgs.neofetch
-          pkgs.git
-          pkgs.iterm2
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-      # Allow unfree packages
-      nixpkgs.config.allowUnfree = true;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
-    };
-  in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#irisdeMac-Pro
     darwinConfigurations."irisdeMac-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        ./mac/pkgs.nix
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
