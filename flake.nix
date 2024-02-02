600adf7181d4bc473f112e4a235f0dc37a03aba5
Glassy Iris <Coloredflowers@outlook.com>
2024-02-02 16:58:23 +0800

    update: atuin logic

--- flake.nix
+++ flake.nix
@@ -13,14 +13,24 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#irisdeMac-Pro
-    darwinConfigurations."irisdeMac-Pro" =
-      nix-darwin.lib.darwinSystem {
+    darwinConfigurations."irisdeMac-Pro" = nix-darwin.lib.darwinSystem {
         specialArgs = { inherit inputs; };
         modules = [
           home-manager.darwinModules.home-manager
           ./mac
         ]; 
+    };
+
+    nixosConfigurations = {
+      "laby" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # 这里导入之前我们使用的 configuration.nix，这样旧的配置文件仍然能生效
+          # 注: configuration.nix 本身也是一个 Nixpkgs Module，因此可以直接在这里导入
+          ./configuration.nix
+        ];
       };
+    };
 
     # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."irisdeMac-Pro".pkgs;
